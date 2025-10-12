2b29647c66b65be903b66106cbacc541337a63bf
taqiyard <taqiyardan19@gmail.com>
2025-10-12 21:16:21 +0700

    save local changes before rebase

--- backend/index.js
+++
@@ -1,2 +0,0 @@
-let nama = "taqi";
-console.log(nama);
--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ app.use(cors());
 // Koneksi Database MySQL
 const db = mysql.createConnection({
     host: "localhost",
-    user: "taqiyard",
-    password: "anakilang123",
+    user: "root",
+    password: "",
     database: "akademi_valorant"
 });
 
